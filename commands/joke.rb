552e329f92755d387c1e2e9a4c357da215ae355c
Pawel Mikolajewski <pawel@cubiware.com>
2011-03-24 13:19:56 +0100

    jokes

---
+++ commands/joke.rb
@@ -0,0 +1,36 @@
+class Commands
+  
+  class Joke
+    
+    def self.command_name
+      'joke'
+    end
+    
+    def self.description
+      "tell typical programmer joke"
+    end
+    
+    def self.syntax
+      'joke'
+    end
+    
+    def self.active?
+      true
+    end
+    
+    def execute(*args)
+      tell_joke
+    end
+    
+    private
+    
+    def	tell_joke
+      str = File.read("jokes.txt")
+      str.squeeze!("#").squeeze!(" ")
+      jokes = str.split("#")
+      jokes[rand(jokes.size)].strip
+    end
+  end
+  
+end
+
